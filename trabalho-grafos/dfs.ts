018c29c84fcb43061981cb4e1fbabd5e1444e3ba
MuriWolf <murillop.o06@gmail.com>
2025-11-22 02:18:10 -0300

    feat: Logic of both functions (dfs and cycle seacher) done

--- trabalho-grafos/dfs.ts
+++ trabalho-grafos/dfs.ts
@@ -1,13 +1,18 @@
-import { graph1 } from "./graph-examples";
-import { Graph } from "./lista-adjacencia";
+import { graph1, graph2 } from "./graph-examples";
+import Graph from "./lista-adjacencia";
 
-function dfs(graph: Graph, statingVertex: string): string[] {
+interface QueueItem {
+    vertex: string,
+    parent: string | undefined
+}
+
+function dfs(graph: Graph, startingVertex: string): string[] {
   let verticesQueue: string[] = []; // push e pop
   let verticesVisited: string[] = [];
 
-  if (graph.getNeighbors(statingVertex).length === 0) return []
+  if (graph.getNeighbors(startingVertex).length === 0) return []
 
-  verticesQueue.push(statingVertex);
+  verticesQueue.push(startingVertex);
   while (verticesQueue.length > 0) {
     const vertexVisited = verticesQueue.pop();
 
@@ -30,5 +35,43 @@ function dfs(graph: Graph, statingVertex: string): string[] {
   return verticesVisited;
 }
 
-const vertices2 = dfs(graph1 , "V1");
-console.log(vertices2);
+function searchCycle(graph: Graph, startingVertex: string): string {
+  let verticesQueue: QueueItem[] = []; // push e pop
+  let verticesVisited: string[] = [];
+
+  if (graph.getNeighbors(startingVertex).length === 0) return "Nenhum ciclo foi encontrado.";
+
+  verticesQueue.push({ vertex: startingVertex, parent: undefined });
+  while (verticesQueue.length > 0) {
+    const vertexVisited: QueueItem | undefined = verticesQueue.pop();
+
+    if (vertexVisited?.vertex) {
+      verticesVisited.push(vertexVisited.vertex);
+
+      let neighbors = graph.getNeighbors(vertexVisited.vertex);
+
+      for (const neighbor of neighbors) {
+        if (
+          !verticesQueue.some(v => v.vertex == neighbor) &&
+          !verticesVisited.includes(neighbor)
+        ) {
+          verticesQueue.push({ vertex: neighbor, parent: vertexVisited.vertex });
+        } else if (neighbor != vertexVisited.parent) {
+          return "Ciclo foi detectado.";
+        }
+      }
+    }
+  }
+
+  return "Nenhum ciclo encontrado.";
+}
+
+// dfs(graph1 , "V1");
+// console.log(vertices);
+
+const graphHasCycle = searchCycle(graph1, "V4"); // yeah, is does :D
+console.log(graphHasCycle);
+
+const graphHasCycle2 = searchCycle(graph2, "V1"); // sorry, but, nope :(
+console.log(graphHasCycle2);
+
